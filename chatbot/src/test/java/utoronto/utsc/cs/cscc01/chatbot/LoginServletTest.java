80d80111c6af2d222d11d29dc5095f222f782120
chrling <chr.ling@alumni.utoronto.ca>
2019-06-17 00:44:57 -0400

    PROJ14-35 finished unit test for login and created class for testing logout

--- chatbot/src/test/java/utoronto/utsc/cs/cscc01/chatbot/LoginServletTest.java
+++ chatbot/src/test/java/utoronto/utsc/cs/cscc01/chatbot/LoginServletTest.java
@@ -1,17 +1,55 @@
 package utoronto.utsc.cs.cscc01.chatbot;
 
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+import java.io.IOException;
+import java.security.Principal;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class LoginServletTest {
+  
+  private UserDatabase db;
+  private LoginServlet servlet;
   @Before
-  public void setUp() throws LifecycleException {
-    Tomcat tomcat = new Tomcat();
-    tomcat.setPort(8080);
-    tomcat.start();
+  public void setUp() {
+    db = new MockDatabase();
+    servlet = new LoginServlet(db);
+  }
+  
+  @Test
+  public void testUserLoginValid() throws ServletException, IOException {
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    HttpServletResponse mockResponse = mock(HttpServletResponse.class);
+    Principal principal = mock(Principal.class);
     
-    UserDatabase db = new MockDatabase();
-    tomcat.addContext("/loginPage", "LoginServlet");
+    when(mockRequest.getParameter("username")).thenReturn("JohnDoe");
+    when(mockRequest.getParameter("password")).thenReturn("abc123");
+    when(mockRequest.getUserPrincipal()).thenReturn(principal);
+    
+    servlet.doPost(mockRequest, mockResponse);
+    
+    verify(mockResponse, atLeastOnce()).sendRedirect("/adminpage");
+  }
+  
+  @Test
+  public void testUserLoginInvalid() throws ServletException, IOException{
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    HttpServletResponse mockResponse = mock(HttpServletResponse.class);
+    Principal principal = mock(Principal.class);
+    RequestDispatcher reqDisp = mock(RequestDispatcher.class);
+    
+    when(mockRequest.getParameter("username")).thenReturn("FakeName");
+    when(mockRequest.getParameter("password")).thenReturn("asdfasdfasdf");
+    when(mockRequest.getUserPrincipal()).thenReturn(principal);
+    when(mockRequest.getRequestDispatcher("/login.jsp")).thenReturn(reqDisp);
+    
+    servlet.doPost(mockRequest, mockResponse);
+    
+    verify(mockRequest, atLeastOnce()).getRequestDispatcher("/login.jsp");
+    
   }
 }
---
+++ chatbot/src/test/java/utoronto/utsc/cs/cscc01/chatbot/LogoutServletTest.java
@@ -0,0 +1,15 @@
+package utoronto.utsc.cs.cscc01.chatbot;
+
+import java.io.IOException;
+import java.security.Principal;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class LogoutServletTest {
+  
+}
