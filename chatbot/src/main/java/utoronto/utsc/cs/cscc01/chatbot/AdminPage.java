1437f97b48a6b3528e09307fe31b891684920cdb
chrling <chr.ling@alumni.utoronto.ca>
2019-06-16 22:52:28 -0400

    PROJ14-28 created base code for handling user login

---
+++ chatbot/src/main/java/utoronto/utsc/cs/cscc01/chatbot/AdminPage.java
@@ -0,0 +1,36 @@
+package utoronto.utsc.cs.cscc01.chatbot;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/adminpage")
+public class AdminPage extends HttpServlet {
+
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    Principal principal = req.getUserPrincipal();
+    if (principal == null) {
+      LoginServlet.forwardToLoginPage(req, resp,
+          "User authentication is required.");
+      return;
+    }
+    resp.setContentType("text/html");
+    PrintWriter writer = resp.getWriter();
+    writer.println("This is the secured portion of the web app");
+    writer.printf("<br/>User: " + req.getRemoteUser());
+    writer.println("<br/><a href='/logout'>Logout</a>");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    // in practice we will call doPost from the webApp but doGet is executed
+    doGet(req, resp);
+  }
+}
---
+++ chatbot/src/main/java/utoronto/utsc/cs/cscc01/chatbot/LoginServlet.java
@@ -0,0 +1,52 @@
+package utoronto.utsc.cs.cscc01.chatbot;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/loginPage")
+/*
+ * This class handles the user logins and 
+ */
+public class LoginServlet extends HttpServlet {
+  // dependency injection of database into login servlet so we can test it
+  private UserDatabase db;
+  
+  public LoginServlet(UserDatabase db) {
+    this.db = db;
+  }
+  
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    // grab username and password from the fields
+    String username = req.getParameter("username");
+    String password = req.getParameter("password");
+    try {
+      req.login(username, password);
+      if (req.getUserPrincipal() != null && db.verifyUser(username, password)) {
+        resp.sendRedirect("/adminpage");
+      }
+      else {
+        // if we couldn't verify the user, try again
+        forwardToLoginPage(req, resp, "Incorrect user name or password.");
+      }
+      
+    } catch (ServletException e) {
+      // System.out.println(e.getMessage());
+      forwardToLoginPage(req, resp, e.getMessage());
+    }
+  }
+
+  public static void forwardToLoginPage(HttpServletRequest req,
+      HttpServletResponse resp, String errorMessage)
+      throws ServletException, IOException {
+
+    req.setAttribute("error", errorMessage);
+    // for now we use this basic login page
+    req.getRequestDispatcher("/login.jsp").forward(req, resp);
+  }
+}
