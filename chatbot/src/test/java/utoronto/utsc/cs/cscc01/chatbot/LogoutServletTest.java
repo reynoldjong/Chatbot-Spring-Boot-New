712164fd7a6e7dadc789568c1f4363f373a5e378
chrling <chr.ling@alumni.utoronto.ca>
2019-06-17 01:26:03 -0400

    PROJ14-35 finished writing test case for logout servlet

--- chatbot/src/test/java/utoronto/utsc/cs/cscc01/chatbot/LogoutServletTest.java
+++ chatbot/src/test/java/utoronto/utsc/cs/cscc01/chatbot/LogoutServletTest.java
@@ -1,8 +1,6 @@
 package utoronto.utsc.cs.cscc01.chatbot;
 
 import java.io.IOException;
-import java.security.Principal;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,4 +10,20 @@ import static org.mockito.Mockito.*;
 
 public class LogoutServletTest {
   
+  private LogoutServlet servlet;
+  
+  @Before
+  public void setUp() {
+    servlet = new LogoutServlet();
+  }
+  
+  @Test
+  public void testLogout() throws ServletException, IOException {
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    HttpServletResponse mockResponse = mock(HttpServletResponse.class);
+    
+    servlet.doGet(mockRequest, mockResponse);
+    
+    verify(mockRequest, atLeastOnce()).logout();
+  }
 }
