630ee19ef017e82fb4ed979c4a60d5ba7ae2363e
logan.corey <logan.corey@mail.utoronto.ca>
2019-06-28 12:20:08 -0400

    PROJ14-43 added divs for chatbot sections Header, Body, Send to Chatbot.js and classes for styling

--- chatbot/src/main/webapp/src/containers/Chatbot/Chatbot.js
+++ chatbot/src/main/webapp/src/containers/Chatbot/Chatbot.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Chatbot.module.css';
-
+import robot from './images/robot.png';
 
 class Chatbot extends React.Component{
     
@@ -12,6 +12,22 @@ class Chatbot extends React.Component{
     render(){
         return(
             <div className={classes.Chatbot}>
+
+                <div className={classes.Header + " blue"}>
+                    <div className={classes.Robot}>
+                        <img src={robot} width="40px" alt=""/>
+                    </div>
+                        <h4 className={classes.Title}>Chatbot</h4> 
+                </div>
+                <div className={classes.Body}>
+                   </div>
+
+                   <div classname={classes.Send}>
+                       <label for="send" style={{float:'left', marginLeft:'4px'}}>Send message</label>
+                       <input name="send" ></input>
+                       </div>
+
+
 
             </div>
         );
