a93ca7893e76188b653a705178dd468f945b16c9
logan.corey <logan.corey@mail.utoronto.ca>
2019-06-28 13:20:53 -0400

    PROJ14-43 Added option to send messages to chatbot window

--- chatbot/src/main/webapp/src/containers/Chatbot/Chatbot.js
+++ chatbot/src/main/webapp/src/containers/Chatbot/Chatbot.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import classes from './Chatbot.module.css';
 import robot from './images/robot.png';
-
+import Message from './Message/Message';
 class Chatbot extends React.Component{
     
     state = {
-        chatlog:[],
+        chatlog:["test message", "Another message"],
     };
 
+    sendMessage = (event) =>{
+        event.preventDefault();
+        const target = event.target;
+        const newMessage = target.elements.send.value;
+
+        let newChatlog = [...this.state.chatlog];
+        newChatlog.push(newMessage);
+
+        this.setState({
+            chatlog:newChatlog,
+        })
+        
+    }
 
     render(){
         return(
@@ -20,11 +33,18 @@ class Chatbot extends React.Component{
                         <h4 className={classes.Title}>Chatbot</h4> 
                 </div>
                 <div className={classes.Body}>
+                   { this.state.chatlog.map((item, i) =>{
+                    return <Message key={i} text={item}/>
+                   }
+                    
+                    ) }
                    </div>
 
                    <div classname={classes.Send}>
-                       <label for="send" style={{float:'left', marginLeft:'4px'}}>Send message</label>
-                       <input name="send" ></input>
+                       <form onSubmit={this.sendMessage}>
+                       
+                       <input  name="send" ></input>
+                       </form>
                        </div>
 
 
--- chatbot/src/main/webapp/src/containers/Chatbot/Message/Message.js
+++ chatbot/src/main/webapp/src/containers/Chatbot/Message/Message.js
@@ -4,7 +4,7 @@ import classes from './Message.module.css';
 const message = (props) => {
     return(
         <div className={classes.Message}>
-            <p>props.text</p>
+            <p>{props.text}</p>
         </div>
     );
 };
