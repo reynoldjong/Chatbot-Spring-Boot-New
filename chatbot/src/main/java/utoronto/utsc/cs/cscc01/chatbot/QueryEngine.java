b4d63337505378ad608aa0c4ec71714d8821b1ae
chrling <chr.ling@alumni.utoronto.ca>
2019-06-29 22:07:21 -0400

    PROJ14-42 simple query function in QueryEngine now distinguishes if result is found in uploaded docs or from web

--- chatbot/src/main/java/utoronto/utsc/cs/cscc01/chatbot/QueryEngine.java
+++ chatbot/src/main/java/utoronto/utsc/cs/cscc01/chatbot/QueryEngine.java
@@ -1,8 +1,10 @@
 package utoronto.utsc.cs.cscc01.chatbot;
 
+import java.util.List;
 import com.ibm.cloud.sdk.core.service.security.IamOptions;
 import com.ibm.watson.discovery.v1.Discovery;
 import com.ibm.watson.discovery.v1.model.Collection;
+import com.ibm.watson.discovery.v1.model.FederatedQueryOptions;
 import com.ibm.watson.discovery.v1.model.GetCollectionOptions;
 import com.ibm.watson.discovery.v1.model.ListCollectionFieldsResponse;
 import com.ibm.watson.discovery.v1.model.ListFieldsOptions;
@@ -25,19 +27,39 @@ public class QueryEngine {
     
     String environmentId = wdisc.getEnvironmentId();
     String crawlerCollectionId = wdisc.getCrawlerCollectionId();
+    String uploadedFilesCollectionId = wdisc.getUploadedFilesCollectionId();
     // query result of crawler for now, hard coded
-    QueryOptions.Builder queryBuilder = new QueryOptions.Builder(environmentId, crawlerCollectionId);
-    queryBuilder.query("enriched_text.concepts.text:" + q);
-    QueryResponse queryResponse = discovery.query(queryBuilder.build()).execute().getResult();
-    System.out.println(queryResponse.getMatchingResults());
-    //System.out.println(queryResponse.getPassages().size());
-    return "hello"; //queryResponse.getPassages().get(0).getPassageText();
+    FederatedQueryOptions.Builder queryBuilder = new FederatedQueryOptions.Builder(environmentId);
+    queryBuilder.naturalLanguageQuery(q);
+    queryBuilder.collectionIds(crawlerCollectionId + "," + uploadedFilesCollectionId);
+    queryBuilder.passages(true);
+    QueryResponse queryResponse = discovery.federatedQuery(queryBuilder.build()).execute().getResult();
+
+    List<QueryResult> resultList = queryResponse.getResults();
+    String result = "";
+    // first check to see if we have any results
+    if (resultList.size() > 0) {
+      QueryResult firstResult = resultList.get(0);
+      // if this was from an uploaded file
+      if (firstResult.getCollectionId().equals(wdisc.getUploadedFilesCollectionId())) {
+        result = "This was from an uploaded file";
+      }
+      // if this was from crawler indexing the webpage
+      else if (firstResult.getCollectionId().equals(wdisc.getCrawlerCollectionId())) {
+        result = "This was found on the web";
+      }
+    }
+    
+    List<QueryPassages> passageList = queryResponse.getPassages();
+    
+    return result;
   }
 
   public static void main(String[] args) {
     WatsonDiscovery watsonDiscovery = WatsonDiscovery.buildDiscovery();
     QueryEngine qe = new QueryEngine(watsonDiscovery);
     
-    System.out.println(qe.simpleQuery("Finance"));
+    System.out.println(qe.simpleQuery("What is context switch?"));
+    
   }
 }
