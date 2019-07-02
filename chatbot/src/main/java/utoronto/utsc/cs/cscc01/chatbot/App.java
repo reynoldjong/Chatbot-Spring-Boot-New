e069961ebe94ebb1895812e7b153d7418781a5e0
Chris Ling <lingchr1@iits-i406-44.utsc-labs.utoronto.ca>
2019-07-02 15:00:22 -0400

    added interface for SearchEngine that will be implemented by QueryEngine

--- chatbot/src/main/java/utoronto/utsc/cs/cscc01/chatbot/App.java
+++ chatbot/src/main/java/utoronto/utsc/cs/cscc01/chatbot/App.java
@@ -1,36 +1,10 @@
-/*package utoronto.utsc.cs.cscc01.chatbot;
-
-
-import java.io.File;
-import javax.servlet.ServletException;
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
+package utoronto.utsc.cs.cscc01.chatbot;
 
 public class App {
-  public static void main(String[] args)
-      throws ServletException, LifecycleException {
-    
-    Tomcat tomcat = TomcatServer.createTomcatServer();
-    
-    QueryServlet query = new QueryServlet();
-    
-    String contextPath = "/";
-    String docBase = new File(".").getAbsolutePath();
-    
-    Context context = tomcat.addContext(contextPath, docBase);
-    
-    String servletName = "MyQuery";
-    String urlPattern = "/QueryServlet";
-    
-    // hard coding for now for live testing, will make web.xml later
-    tomcat.addServlet(contextPath, servletName, query);
-    context.addServletMapping(urlPattern, servletName);
-    
-    // start the server
-    tomcat.start();
-    tomcat.getServer().await();
+  public static void main(String[] args) {
 
+	System.out.println("Hello World");
+	WatsonDiscovery wd = WatsonDiscovery.buildDiscovery();
+	SearchEngine queryEngine = new QueryEngine(wd);
   }
 }
-*/
