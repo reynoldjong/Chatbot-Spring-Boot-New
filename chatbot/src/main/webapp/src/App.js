f93808748d1cfecfe4b4c9f23e62ede7574f2c3a
logan.corey <logan.corey@mail.utoronto.ca>
2019-07-06 16:50:38 -0400

    transformed App.js into a container

--- chatbot/src/main/webapp/src/App.js
+++ chatbot/src/main/webapp/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Chatbot from './containers/Chatbot/Chatbot';
 import './App.css';
 
@@ -7,8 +7,11 @@ import Hero from './components/Hero/Hero';
 import LogoBar from './components/Navbar/LogoBar/LogoBar';
 import MiddleNav from './components/Navbar/MiddleNav/MiddleNav';
 import Footer from './components/Footer/Footer';
-function App() {
-  return (
+
+
+class App extends Componenet {
+  render(){
+    return (
     <div className="App">
       <LogoBar/>
       <Navbar/>
@@ -19,6 +22,7 @@ function App() {
     </div>
   
   );
+}
 }
 
 export default App;
