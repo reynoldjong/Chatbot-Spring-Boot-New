d424194eeec766fffad767eb210c15a30490fa2b
Logan Corey <logan.corey@mail.utoronto.ca>
2019-07-07 08:16:10 -0700

    Added Features section to home page and reduce font-size of sections by .1em

--- chatbot/frontend/src/components/Home/Home.js
+++ chatbot/frontend/src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from './Hero/Hero';
 import AboutUs from './AboutUs/AboutUs';
+import Features from './Features/Features';
 const home = () =>{
     return(
         <React.Fragment>
             <Hero/>
             <div className='container'>
             <AboutUs/>
+            <Features/>
             </div>
            
         </React.Fragment>
