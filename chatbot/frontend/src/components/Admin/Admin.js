127211e7ae3b53ab3a7fe71c00f11dbd1d5d21be
Logan Corey <logan.corey@mail.utoronto.ca>
2019-07-07 10:26:36 -0700

    PROJ14-33 added controls to admin dashboard

--- chatbot/frontend/src/components/Admin/Admin.js
+++ chatbot/frontend/src/components/Admin/Admin.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import classes from './Admin.module.css';
-import Sidebar from './Sidebar/Sidebar';;
+import Sidebar from './Sidebar/Sidebar';
 import AdminControls from './AdminControls/AdminControls';
 
 const admin = (props) =>{
     let admin = null;
-    console.log(props);
+   
     if(props.loggedIn){
         admin = (
             <React.Fragment>
-
+                
            <div className={classes.Sidebar}>
+               
                <Sidebar/>
-               <AdminControls/>
+              
                </div>
             
             <div className={classes.Content}>
+                <div className='container'>
 
+            <h2> Add or Remove a File</h2>
+            <AdminControls/>
+            </div>
             </div>
                 </React.Fragment>
         );
