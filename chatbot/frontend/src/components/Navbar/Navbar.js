d8110575eb81f3d70645832ede35487dc94e8fb3
Logan Corey <logan.corey@mail.utoronto.ca>
2019-07-07 08:06:59 -0700

    made navbar transparent and fixed so it sits ontop of the hero image

--- chatbot/frontend/src/components/Navbar/Navbar.js
+++ chatbot/frontend/src/components/Navbar/Navbar.js
@@ -5,19 +5,21 @@ import image from './images/DFILogo.png';
 const navbar = () =>{
     return(
         <React.Fragment>
-            <nav>
-            <div className="nav-wrapper  blue darken-1" >
-            
+            <div className={classes.NavbarFixed}>
+            <nav className='transparent z-depth-0'>
+
+            <div className="nav-wrapper" >
                 <a href="https://www.digitalfinanceinstitute.org/" className={classes.NavbarLogo} > <img src={image} alt="DFI Logo"/> </a>
-    
-         
-            <ul id="nav-mobile" class="right hide-on-med-and-down">
+
+            <ul id="nav-mobile" class="right hide-on-med-and-down" >
                 <li><a href="sass.html">Login</a></li>
                 <li><a href="badges.html">Register</a></li>
                 <li><a href="badges.html">Help</a></li>
             </ul>
+
             </div>
         </nav>
+        </div>
       </React.Fragment>
             
     );
