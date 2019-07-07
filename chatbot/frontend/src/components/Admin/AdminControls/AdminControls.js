e068e9ab687d51d34486f2c1f69ae496229f96a8
Logan Corey <logan.corey@mail.utoronto.ca>
2019-07-07 10:29:36 -0700

    PROJ14-33 added tempory fields for AdminControls component

--- chatbot/frontend/src/components/Admin/AdminControls/AdminControls.js
+++ chatbot/frontend/src/components/Admin/AdminControls/AdminControls.js
@@ -16,9 +16,23 @@ const adminControls = () => {
         </div>
       </div>
       <div className="row">
-        <div className="input-field col s12">
-          <input disabled value="I am not editable" id="disabled" type="text" className="validate"/>
-          <label for="disabled">Disabled</label>
+        <div className="input-field col s6">
+          <input placeholder="Placeholder" id="first_name" type="text" className="validate"/>
+          <label for="first_name">First Name</label>
+        </div>
+        <div className="input-field col s6">
+          <input id="last_name" type="text" className="validate"/>
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s6">
+          <input placeholder="Placeholder" id="first_name" type="text" className="validate"/>
+          <label for="first_name">First Name</label>
+        </div>
+        <div className="input-field col s6">
+          <input id="last_name" type="text" className="validate"/>
+          <label for="last_name">Last Name</label>
         </div>
       </div>
     
