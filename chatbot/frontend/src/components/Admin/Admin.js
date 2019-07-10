2d61cc73a21e82e8a76225a152e99daea0dc3017
logan.corey <logan.corey@mail.utoronto.ca>
2019-07-09 20:36:17 -0400

    added styling to navbar, shadow, dashboard section, changed font to avenure

--- chatbot/frontend/src/components/Admin/Admin.js
+++ chatbot/frontend/src/components/Admin/Admin.js
@@ -1,40 +1,22 @@
-import React from 'react';
-import classes from './Admin.module.css';
-import Sidebar from './Sidebar/Sidebar';
-import AdminControls from './AdminControls/AdminControls';
-import Navbar from './Navbar/Navbar';
-const admin = (props) =>{
-    let admin = null;
-   
-    if(props.loggedIn){
-        document.body.style = 'background: rgba(0,0,0,0.05);';
-        admin = (
-            
-            <React.Fragment>
-       <Navbar/>
-          
-               
-               <Sidebar/>
-              
-           
-          
-        
-                </React.Fragment>
-        );
-    }
-    else{
-        admin = <h2>Not Logged in</h2>
-    }
+import React from "react";
+import classes from "./Admin.module.css";
+import AdminControls from "./AdminControls/AdminControls";
+import Navbar from "./Navbar/Navbar";
+const admin = props => {
+  let admin = null;
 
+  if (props.loggedIn) {
+    document.body.style = "background: rgba(0,0,0,0.05);";
+    admin = (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
+  } else {
+    admin = <h2>Not Logged in</h2>;
+  }
 
-    return(
-        <div className={classes.Admin}>
-            {admin}
-        </div>
-      
-       
-    )
-
-}
+  return <div className={classes.Admin}>{admin}</div>;
+};
 
 export default admin;
--- chatbot/frontend/src/components/Admin/Navbar/Navbar.js
+++ chatbot/frontend/src/components/Admin/Navbar/Navbar.js
@@ -6,14 +6,19 @@ const navbar = () =>{
 
      
         <nav className={classes.Navbar}>
-        <div className={classes.Navbar + " nav-wrapper  blue-grey darken-3"}>
-          <a  style={{position:'relative',float:'left', marginLeft:'40px'}}href="www.google.com" className="brand-logo">DFI Cloud</a>
+        <div className={ " nav-wrapper  blue-grey darken-4"}>
+          <a  style={{position:'relative',float:'left', marginLeft:'20px'}}href="www.google.com" className="brand-logo">DFI Cloud</a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Docs</a></li>
-            <li><a href="badges.html">Support</a></li>
-            <li><a href="collapsible.html">Resources</a></li>
+          <li><a href="/">Home</a></li>
+            <li><a href="sass.html">Documents</a></li>
+            <li><a href="badges.html">Users</a></li>
+            <li><a href="collapsible.html">Statistics</a></li>
           </ul>
+         
         </div>
+        <div className={classes.Dashboard + ' z-depth-1'}>
+              <h5>Dashboard</h5>
+          </div>
       </nav>
       </React.Fragment>
     );
