b4c6d31f89f155369e72da0b1a7c9fc780387238
chrling <chr.ling@alumni.utoronto.ca>
2019-08-05 16:31:05 -0400

    more unit tests and removed unused classes

---
+++ chatbot/src/test/java/utoronto/utsc/cs/cscc01/chatbot/QueryAssistantTest.java
@@ -0,0 +1,40 @@
+package utoronto.utsc.cs.cscc01.chatbot;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QueryAssistantTest {
+  private QueryAssistant qa;
+
+  @Before
+  public void setUp() {
+    this.qa = new QueryAssistant(WatsonAssistant.buildAssistant());
+  }
+
+  // test to see if we are returned a hashtable of correct format
+  @Test
+  public void testAssistantQuery() {
+    String query = "hello world";
+    Hashtable<String, ArrayList<String>> result =
+        qa.simpleAssistantQuery(query);
+    // we can't test the content because that may change
+    // so we test the format
+    assertTrue(result.containsKey("text"));
+    assertTrue(result.containsKey("image"));
+    assertTrue(result.containsKey("url"));
+    assertTrue(result.containsKey("queryFlag"));
+  }
+
+  // query that assistant cannot answer should return "Need to query"
+  @Test
+  public void testAssistantInvalidQuery() {
+    String query = "adfgjhaeltuqwnokehrtiawg587q=tr8q3trfguawgruja";
+    Hashtable<String, ArrayList<String>> result =
+        qa.simpleAssistantQuery(query);
+    assertEquals("Need to query", result.get("queryFlag").get(0));
+  }
+}
